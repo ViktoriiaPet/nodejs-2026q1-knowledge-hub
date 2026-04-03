04c92cdaa6e4ebc599313344cf6180fd0674d0c2
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-04-03 10:04:34 +0200

    fix: update imports in dto comment

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsOptional, IsIn, isString } from "class-validator";
+import { IsString, IsNotEmpty, IsOptional, IsIn } from "class-validator";
 export class CreateCommentDto {
        @IsString()
     @IsNotEmpty()
