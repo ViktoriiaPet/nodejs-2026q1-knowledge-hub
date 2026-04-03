0e93a8b31ae501748bbf17d4970c1a4134127f3e
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-04-03 08:33:03 +0200

    chore: add interfaces for CRUDs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import type { User } from 'src/types';
 
 @Injectable()
 export class UsersService {
+  private users: User[] = [];
   create(createUserDto: CreateUserDto) {
+    
     return 'This action adds a new user';
   }
 
