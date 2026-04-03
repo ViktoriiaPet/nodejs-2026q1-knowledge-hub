ba9dc0e1c5b989b7fd955e67f4b9a592849a6970
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-04-03 10:04:05 +0200

    feat: add primary post structure to category part

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { Category } from 'src/types';
+import type { Category } from 'src/types';
+import { randomUUID } from 'node:crypto';
 
 @Injectable()
 export class CategoriesService {
   private categories: Category[] = []
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  create(createCategoryDto: CreateCategoryDto) : Category {
+    const category: Category = {
+      id: randomUUID(),
+      name: createCategoryDto.name,
+      description: createCategoryDto.description
+    }
+    this.categories.push(category)
+    return category
   }
 
   findAll() {
